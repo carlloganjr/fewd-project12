10720063a463c018d0c1fe7cb0f695495b68d4c4
carlloganjr <carlloganjr@gmail.com>
2017-09-21 01:54:20 -0700

    added about section, lazyloading images, and on scroll event

--- src/js/app.js
+++ src/js/app.js
@@ -36,3 +36,35 @@ menu.addEventListener('click', function(e) {
     }, 600);
   }
 });
+
+$(window).scroll(function() {
+  $('.proj').each(function(i) {
+    const projects = document.getElementsByClassName('proj');
+    const y_scroll = window.scrollY;
+    const win_ht = window.innerHeight;
+    const folio_offset = projects[i].offsetTop;
+    let bg_img = [
+      'url(img/dir.png)',
+      'url(img/video.png)',
+      'url(img/dash.png)',
+      'url(img/form.png)',
+      'url(img/gallery.png)'
+    ]
+    let y_start = win_ht + folio_offset - (win_ht * .1);
+    let bg_start = win_ht + folio_offset - (win_ht * .5);
+    let menu_bg = win_ht - 55;
+    if(y_start <= y_scroll) {
+      $(this).next().children().children('svg').css('margin-top', '1.25rem');
+    } else {
+      $(this).next().children().children('svg').css('margin-top', '100%');
+    }
+    if(bg_start <= y_scroll) {
+      $(this).next().css('background-image', bg_img[i]);
+    }
+    if(menu_bg <= y_scroll) {
+      $('.menu_bg').css('height', '55px');
+    } else {
+      $('.menu_bg').css('height', '0');
+    }
+  });
+});
